41a21e6b0a8990c9191cc48519295971fca2f57e
mirko <mirko.vanzetto@edu.unito.it>
2022-12-13 13:09:50 +0100

    inserito le var/costanti che sono da usare, migliorare il commento di SO_LOADSPEED

--- Utility.c
+++ Utility.c
@@ -1,3 +1,31 @@
 //
-// Created by vboxuserpasciu on 12/13/22.
+// Created by daddy on 12/13/22.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+
+
+/* Tutti i dati che saranno condivisi con gli altri processi*/
+
+#define SO_NAVI 1 //numero di navi che navigano
+#define SO_PORTI 4 //numero di porti presenti
+#define SO_MERCI 2 //tipi di merci diverse
+#define SO_SIZE 1 //tonnellate di merci
+#define SO_MIN_VITA 10 //giorni di vita  MIN della merce
+#define SO_MAX_VITA 30 //giorni di vita  MAX della merce
+#define SO_LATO 10.0 //lunghezza del lato della mappa (quadrata)
+#define SO_SPEED 1.0 //KM AL GIORNO
+#define SO_CAPACITY 10 //Tonnellate che può caricare ogni nave
+#define SO_BANCHINE 3 // Banchine che ha ogni porto
+#define SO_FILL 200000 //Tonnellate totali di merci richieste e offerte da TUTTI i porti in totale
+#define SO_LOADSPEED 200 //tonnellate al giorno
+#define SO_DAYS 10 //giorni dopo quanto muore il processo
