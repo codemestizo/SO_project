4fce4ad77dafbd253554881279493db597028d1f
mirko <mirko.vanzetto@edu.unito.it>
2022-12-13 12:38:14 +0100

    processi

---
+++ nave.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+
+/* Processo nave */
+
