4ca5a23a89543ed7a129f34f7882de05005b30dd
zombieFireITA <61471823+zombieFireITA@users.noreply.github.com>
2023-01-05 11:11:36 +0100

    Update utility.h

--- utility.h
+++ utility.h
@@ -6,15 +6,15 @@
 
 #define SO_NAVI 2 //numero di navi che navigano
 #define SO_PORTI 5 //numero di porti presenti
-#define SO_MERCI 2 //tipi di merci diverse
-#define SO_SIZE 1 //tonnellate di merci
+#define SO_MERCI 10 //tipi di merci diverse
+#define SO_SIZE ( SO_FILL/SO_PORTI) //tonnellate di merci
 #define SO_MIN_VITA 10 //giorni di vita  MIN della merce
 #define SO_MAX_VITA 30 //giorni di vita  MAX della merce
 #define SO_LATO 30.0 //lunghezza del lato della mappa (quadrata)
 #define SO_SPEED 4.0 //KM AL GIORNO
 #define SO_CAPACITY 10 //Tonnellate che può caricare ogni nave
 #define SO_BANCHINE 3 // Banchine che ha ogni porto
-#define SO_FILL 200000 //Tonnellate totali di merci richieste e offerte da TUTTI i porti in totale
+#define SO_FILL 500 //Tonnellate totali di merci richieste e offerte da TUTTI i porti in totale
 #define SO_LOADSPEED 200 //tonnellate al giorno per cui viene impegnata una banchina // velocità carico/scarico
 #define SO_DAYS 10 //giorni dopo quanto muore la simulazione
 #define SO_MERCI_NAVE 1 //merci richieste dalla singola nave
@@ -54,19 +54,29 @@ typedef struct {
     int semIdBanchinePorto;
     structMerce merce[SO_MERCI];
 }portDefinition;
-
+static int lungfifo;
+static char fifo_name1[] = "reportFifo";
+static  int fd;
 static struct msgbuf buf;
 static portDefinition *portArrays;
 static int portArrayId;
 static int semPortArrayId;
+static int semMessageQueueId;
 static int messageQueueId;
-static key_t keyPortArray;
-static key_t keyMessageQueue;
-static key_t keySemPortArray;
+static key_t keyPortArray=6050;
+static key_t keyMessageQueue=5050;
+static int keySemMessageQueue;
+static int keySemPortArray;
 static int *array;
 static int shmid;
 static int giorniSimulazione=0;
+static int posizioneMerce=0;
+static int leng=SO_PORTI*SO_MERCI;
+
+static int sum; //usato per parificare il tot merci
 //portDefinition * createPortArray();
+void testo();
+void createIPCKeys();
 
 int controlloPosizione( int x, int y);
 
