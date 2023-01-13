94047e03cb41f877b758fc7b4aaf0c1ef6cd4417
zombie <61471823+zombieFireITA@users.noreply.github.com>
2023-01-13 12:37:28 +0100

    Update utility.h

--- utility.h
+++ utility.h
@@ -6,18 +6,18 @@
 
 #define SO_NAVI 1 //numero di navi che navigano
 #define SO_PORTI 5 //numero di porti presenti
-#define SO_MERCI 4 //tipi di merci diverse
+#define SO_MERCI 8 //tipi di merci diverse
 #define SO_SIZE ( SO_FILL/SO_PORTI) //tonnellate di merci
 #define SO_MIN_VITA 10 //giorni di vita  MIN della merce
 #define SO_MAX_VITA 30 //giorni di vita  MAX della merce
 #define SO_LATO 30 //lunghezza del lato della mappa (quadrata)
-#define SO_SPEED 20 //KM AL GIORNO
-#define SO_CAPACITY 80.0 //Tonnellate che può caricare ogni nave
+#define SO_SPEED 5 //KM AL GIORNO
+#define SO_CAPACITY 1000 //Tonnellate che può caricare ogni nave
 #define SO_BANCHINE 3 // Banchine che ha ogni porto
-#define SO_FILL 500 //Tonnellate totali di merci richieste e offerte da TUTTI i porti in totale
-#define SO_LOADSPEED 200 //tonnellate al giorno per cui viene impegnata una banchina // velocità carico/scarico
-#define SO_DAYS 10 //giorni dopo quanto muore la simulazione
-#define SO_MERCI_NAVE 1 //merci richieste dalla singola nave
+#define SO_FILL 1000 //Tonnellate totali di merci richieste e offerte da TUTTI i porti in totale
+#define SO_LOADSPEED 500 //tonnellate al giorno per cui viene impegnata una banchina // velocità carico/scarico
+#define SO_DAYS 10//giorni dopo quanto muore la simulazione
+#define SO_MERCI_NAVE 2 //merci richieste dalla singola nave
 #define MSG_LEN 200
 
 union semun {
@@ -71,6 +71,7 @@ typedef struct {
 static int lungfifo;
 static char fifo_name1[] = "reportFifo";
 static  int fd;
+static struct msgbuf *buf;
 static portDefinition *portArrays;
 static int portArrayId;
 static int semPortArrayId;
