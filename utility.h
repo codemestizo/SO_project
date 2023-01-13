4fea45c4b03a13f4d639cc2b284e2e4035d851f5
zombie <61471823+zombieFireITA@users.noreply.github.com>
2023-01-13 12:37:12 +0100

    Update utility.h

--- utility.h
+++ utility.h
@@ -5,13 +5,13 @@
 #include <glob.h>
 
 #define SO_NAVI 1 //numero di navi che navigano
-#define SO_PORTI 6 //numero di porti presenti
+#define SO_PORTI 5 //numero di porti presenti
 #define SO_MERCI 4 //tipi di merci diverse
 #define SO_SIZE ( SO_FILL/SO_PORTI) //tonnellate di merci
 #define SO_MIN_VITA 10 //giorni di vita  MIN della merce
 #define SO_MAX_VITA 30 //giorni di vita  MAX della merce
 #define SO_LATO 30 //lunghezza del lato della mappa (quadrata)
-#define SO_SPEED 4 //KM AL GIORNO
+#define SO_SPEED 20 //KM AL GIORNO
 #define SO_CAPACITY 80.0 //Tonnellate che può caricare ogni nave
 #define SO_BANCHINE 3 // Banchine che ha ogni porto
 #define SO_FILL 500 //Tonnellate totali di merci richieste e offerte da TUTTI i porti in totale
